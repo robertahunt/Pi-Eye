4bf9694a2bef917153432f59e5bbb794686d111f
Roberta Hunt <rehunt@ualberta.ca>
2023-01-02 13:45:35 +0100

    added try except

--- src/server.py
+++ src/server.py
@@ -3,14 +3,19 @@ from bottle import route, run
 from picamera2 import Picamera2, Preview
 
 global picam2
-picam2 = Picamera2()
-picam2.start()
+try:
+    picam2 = Picamera2()
+    picam2.start()
+except:
+    pass
 
 @route('/reinitCamera')
 def reinit_camera():
     global picam2
     try:
         picam2.close()
+    except:
+        pass
 
     picam2 = Picamera2()
     #camera_config = picam2.create_still_configuration(raw={}, main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
@@ -19,11 +24,8 @@ def reinit_camera():
 
 @route('/getPhoto')
 def get_photo():
-    try:
-    	arr = picam2.capture_image("lores")
-    	return {'photo':str(arr[0][0])}
-    except:
-        
+    arr = picam2.capture_image("lores")
+    return {'photo':str(arr[0][0])}
 
 @route('/hello')
 def hello():
