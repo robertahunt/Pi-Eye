e7ef32cf30c57b6cac5e272b374e38709d2d6a7e
Roberta <rehunt@ualberta.ca>
2023-01-05 13:47:27 +0000

    Working version 0.1

--- src/pieye.py
+++ src/pieye.py
@@ -1,5 +1,6 @@
 import time
 import logging
+import numpy as np
 from logging import handlers
 from picamera2 import Picamera2
 from bottle import abort
@@ -7,6 +8,7 @@ from bottle import abort
 class PiEye():
     def __init__(self, debug=False):
         self.logger = self.make_logger(debug)
+        self.image_cache = {} #use dictionary so we can store the 'image_name', to ensure we aren't getting an old file or sth.
         pass
     
     def make_logger(self, debug):
@@ -37,7 +39,7 @@ class PiEye():
             
             self.logger.debug('Setting up configuration..')
             self.preview_config = self.camera.create_preview_configuration()
-            self.image_config = self.camera.create_still_configuration(lores={"size":(320,240)},display='lores')
+            self.image_config = self.camera.create_still_configuration(main={"format":"XRGB8888"},lores={"size":(320,240)},display='lores')
             
             # ensures sizes match sensor sizes and speeds up capture
             self.camera.align_configuration(self.image_config)
@@ -53,18 +55,42 @@ class PiEye():
 
     def check_camera_is_setup(self):
         self.logger.debug('Checking if already set up camera')
-        if hasattr(self, 'camera'):
+        if hasattr(self, 'camera') and self.camera.is_open:
             return True
         else:
             return self.reinit_camera()
         
     def capture_preview(self):
         self.check_camera_is_setup()
-        self.logger.debug('Roberta should implement this')
-        return [[None]]
+        array = self.camera.capture_array("lores")
+        self.logger.debug('Took preview photo')
+        return array
     
     def capture_image(self):
         self.check_camera_is_setup()
-        self.logger.debug('Roberta should implement this')
-        return [[None]]
         
+        now1 = np.datetime64('now')
+        array = self.camera.capture_array("main")
+        now2 = np.datetime64('now')
+        print(now2-now1,'Took to take image')
+        self.logger.debug('Took full resolution image')
+        return array        
+    
+    def capture_image_and_cache(self):
+        self.image_cache = {} #reset cache - only hold one image at a time
+        self.check_camera_is_setup()
+        
+        now = np.datetime64('now')
+        image_name = np.datetime_as_string(now, unit='ms', timezone='UTC')
+        
+        now1 = np.datetime64('now')
+        array = self.camera.capture_array("main")
+        self.image_cache[image_name] = array
+        self.image_cache['latest'] = array #for easy debugging for Roberta
+        now2 = np.datetime64('now')
+        print(now2-now1,'Took to take image')
+        self.logger.debug('Took full resolution image')
+        return image_name        
+    
+    def get_cached_image(self, image_name):
+        return self.image_cache.get(image_name, None)
--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,11 @@
+import io
+import os
+import cv2
+import base64
+import numpy as np
+import matplotlib.pyplot as plt
 from pieye import PiEye
-from bottle import route, run, abort, error
+from bottle import route, run, abort, error, yieldroutes, response, static_file
 
 
 
@@ -13,28 +19,94 @@ def reinit_camera():
 
 @route("/getPreview")
 def get_preview():
-    arr = eye.capture_preview()
-    return {"image": str(arr[0][0])}
+    buf = io.BytesIO()
+    array = eye.capture_preview()
+    array = cv2.cvtColor(array, cv2.COLOR_YUV420sp2RGB)
+    plt.imsave(buf, array, format="jpeg")
+    buf.seek(0)
+    byts = buf.read()
+    response.set_header('Content-type','image/jpeg')
+    return byts
 
 
-@route("/takeImage")
-def take_image():
-    return {}
+@route("/takeImageAndSave")
+def take_image_and_save():
+    array = eye.capture_image()
+    now = np.datetime64('now')
+    fn = np.datetime_as_string(now, unit='ms', timezone='UTC') + '_photo.png'
+    fp = os.path.join('/tmp', fn)
+    #array = cv2.cvtColor(array, cv2.COLOR_RGBA2RGB)
+    
+    now1 = np.datetime64('now')
+    cv2.imwrite(fp, array)
+    
+    now2 = np.datetime64('now')
+    print(now2-now1,'Took to write image to tmp :(')
+    return {'fn': fn}
 
 
-@route("/getImage")
-def get_image():
 
-    arr = eye.capture_image()
-    return {"image": str(arr[0][0])}
+@route("/takeAndCacheImage")
+def take_image_and_cache():
+    now1 = np.datetime64('now')
+    image_name = eye.capture_image_and_cache()
+    
+    now2 = np.datetime64('now')
+    print(now2-now1,'Took to get and cache image')
+    return {'image_name': image_name}
+
+
+
+@route("/getImage/<filename>")
+def get_image(filename):
+    fp = os.path.join('/tmp',filename)
+    #buf = io.BytesIO()
+    #array = cv2.imread(fp)
+    #plt.imsave(buf, array, format="jpeg")
+    #buf.seek(0)
+    #byts = buf.read()
+    #response.set_header('Content-type','image/jpeg')
+    return static_file(filename, root='/tmp')
+
+
+
+@route("/getCachedImage/<image_name>")
+def get_cached_image(image_name):
+    
+    now1 = np.datetime64('now')
+    array = eye.get_cached_image(image_name)
+    if array is None:
+        abort(590, "Image no longer cached... could not fetch.")
+    buf = io.BytesIO()
+    plt.imsave(buf, array, format="jpeg")
+    buf.seek(0)
+    byts = buf.read()
+    response.set_header('Content-type','image/jpeg')
+    now2 = np.datetime64('now')
+    print(now2-now1,'Took to get and buf image')
+    
+    return byts
 
 
 @route("/takeAndGetImage")
 def take_and_get_image():
-    arr = eye.capture_image()
-    return {"image": str(arr[0][0])}
+    now1 = np.datetime64('now')
+    buf = io.BytesIO()
+    array = eye.capture_image()
+    plt.imsave(buf, array, format="jpeg")
+    buf.seek(0)
+    byts = buf.read()
+    response.set_header('Content-type','image/jpeg')
+    now2 = np.datetime64('now')
+    print(now2-now1,'Took to get and buf image')
+    return byts
 
 
 @route("/hello")
 def hello():
     return "Hello World!"
+
+@route("/")
+def list_routes():
+    
+    return {'valid routes': ["/hello", "/takeAndGetImage", "/getImage", "/takeImage","getPreview","reinitCamera"]}
