11128b09ef1d3b8405929275b5db89313f4e6fc9
Roberta Hunt <rehunt@ualberta.ca>
2023-01-24 14:57:28 +0100

    update setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # install software needed
-sudo apt-get update
-sudo apt-get install git
-sudo apt-get install python3-pip
-sudo apt-get install libatlas-base-dev
-sudo apt install python3-opencv
+sudo apt-get update -y
+sudo apt-get install git -y
+sudo apt-get install python3-pip -y
+sudo apt-get install libatlas-base-dev -y
+sudo apt install python3-opencv -y
 
 # get latest code
 git clone https://github.com/robertahunt/Pi-Eye.git /home/pi/Pi-Eye
@@ -22,3 +22,12 @@ sudo usermod -a -G video pi # after opencv install, seems cannot access camera
 sudo cp Pi-Eye/pieye.service /lib/systemd/system/
 sudo systemctl enable pieye
 sudo systemctl start pieye
+
+# turn on watchdog
+sudo sed -i 's/#RuntimeWatchdogSec=0/RuntimeWatchdogSec=10/' /etc/systemd/system.conf
+sudo sed -i 's/#ShutdownWatchdogSec=10min/ShutdownWatchdogSec=2min/' /etc/systemd/system.conf
+
+# turn on cronjob to update pieye git
+sudo chmod +x Pi-Eye/cron_pull.sh
+(sudo crontab -l && echo "*/15 * * * * /home/pi/Pi-Eye/cron_pull.sh") | sudo crontab -
+
